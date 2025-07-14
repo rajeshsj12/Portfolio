0cd68f60a61df7a6a175c66807195fa84f6afb2a
rajeshsj12 <46038988+rajeshsj12@users.noreply.github.com>
2025-07-14 09:33:30 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: "/Portfolio/", // 👈 Important for GitHub Pages routing
   plugins: [
     react(),
     runtimeErrorOverlay(),
@@ -11,7 +12,7 @@ export default defineConfig({
     process.env.REPL_ID !== undefined
       ? [
           await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
+            m.cartographer()
           ),
         ]
       : []),
